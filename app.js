85029f43f52a61571a72aa4a1ee39e70cdc360b9
robcrock <rob@robcrock.com>
2018-01-26 14:37:18 +1100

    Got one Turkey working.

--- app.js
+++ app.js
@@ -1,42 +1,135 @@
-(function () {
-  const margin = { top: 50, left: 50, right: 50, bottom: 50 },
-    height = 540 - margin.top - margin.bottom,
-    width = 700 - margin.left - margin.right;
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// Margin convention
+////////////////////////////////////////////////////////////////////////////////
 
-  const svg = d3.select('#map')
-    .append('svg')
-    .attr('height', height - margin.top + margin.bottom)
-    .attr('width', width + margin.left + margin.right)
-    .append('g')
-    .attr('transform', `translate( ${margin.left} , ${margin.top} )`);
+const margin = { top: 50, left: 50, right: 50, bottom: 50 },
+  height = 800 - margin.top - margin.bottom,
+  width = 600 - margin.left - margin.right;
 
-  /*
-    Read in world.topojson
-    Read in capiltals.csv
-  */
-  d3.queue()
-    .defer(d3.json, 'data/world.json')
-    .defer(d3.csv, 'https://gist.githubusercontent.com/robcrock/a30f589308f73f234f46986a21676e30/raw/d1a632ced68f1cf115a5ca9af8baafa6920da05d/data_slim.csv')
-    .await(ready);
+const svg = d3.select('#map')
+  .append('svg')
+  .attr('height', height - margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', `translate( ${margin.left} , ${margin.top} )`);
 
-  /*
-    Create a new projection using Orthographic (geoOrthographic)
-    and center it (translate)
-    and zoom in a certain amount (scale)
-  */
-  const projection = d3.geoTwoPointEquidistant([-100, 25], [-70, 25])
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// Bring in raw data
+////////////////////////////////////////////////////////////////////////////////
+
+const csvURL = 'https://gist.githubusercontent.com/robcrock/a30f589308f73f234f46986a21676e30/raw/d1a632ced68f1cf115a5ca9af8baafa6920da05d/data_slim.csv'
+
+d3.queue()
+  .defer(d3.json, 'data/world.json')
+  .defer(d3.csv, csvURL)
+  .await(ready);
+
+function ready(error, world, data) {
+
+  const land = topojson.feature(world, world.objects.ne_50m_land).features;
+  const lakes = topojson.feature(world, world.objects.ne_50m_lakes).features;
+  const rivers = topojson.feature(world, world.objects.ne_50m_rivers_lake_centerlines).features;
+
+  const parseTime = d3.timeParse("%Y-%m-%dT%H:%M:%S");
+
+  data.forEach(function (record) {
+    record.time_integer = parseTime(record.timestamp).getTime();
+  })
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////// Nest and sort data
+  ////////////////////////////////////////////////////////////////////////////////
+
+  const migrationStartTimeOverall = d3.nest()
+    .rollup(function (leaves) { return d3.min(leaves, d => d.time_integer) })
+    .entries(data);
+  const migrationEndTimeOverall = d3.nest()
+    .rollup(function (leaves) { return d3.max(leaves, d => d.time_integer) })
+    .entries(data);
+
+  const timeScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([migrationStartTimeOverall, migrationStartTimeOverall]);
+
+  let nested = d3.nest()
+    .key(d => d.animal_id)
+    .entries(data);
+
+  nested = d3.nest()
+    .key(d => d.animal_id)
+    .rollup(function (leaves) {
+      return {
+        'events': leaves.length,
+        'min_time': d3.min(leaves, d => d.time_integer),
+        'max_time': d3.max(leaves, d => d.time_integer)
+      }
+    })
+    .entries(data);
+
+  nested = d3.nest()
+    .key(d => d.animal_id)
+    .sortValues((a, b) => a.time_integer - b.time_integer)
+    .entries(data);
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////// Create GeoJSON Feature Collection
+  ////////////////////////////////////////////////////////////////////////////////
+
+  const featureCollection = {
+    "type": "FeatureCollection",
+    "features": [ /* append featuresObj */]
+  };
+
+  nested.forEach(function (turkey) {
+
+    let turkeyObj = {
+      "type": "Feature",
+      "geometry": { /* append migrationObj */ },
+      "properties": { /* append turkeyProps */ }
+    };
+    let migrationObj = {
+      "type": "LineString",
+      "coordinates": []
+    };
+    let turkeyProps = {};
+
+    turkey.values.forEach(function (migration) {
+      migrationObj.coordinates.push([+migration.longitude, +migration.latitude])
+    })
+
+    turkeyObj.geometry = migrationObj;
+    turkeyProps.animal_id = turkey.key;
+
+    turkeyObj.properties = turkeyProps;
+
+    featureCollection.features.push(turkeyObj)
+
+  });
+
+  console.log(featureCollection);
+
+  featureCollection.features.forEach(function(d) {
+    console.log(d.properties.animal_id);
+  })
+
+  // console.log(featureCollection);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////// Set up geo projection
+////////////////////////////////////////////////////////////////////////////////
+
+  const projection = d3.geoTwoPointEquidistant([-90, 15], [-60, 15])
     .translate([width / 2, height / 2.65])
     .scale(300);
 
-  /*
-    Create a path (geoPath)
-    using the projection
-  */
   const geoPath = d3.geoPath()
     .projection(projection);
 
-
   const graticule = d3.geoGraticule();
+
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////// Bring element on screen
+////////////////////////////////////////////////////////////////////////////////
 
   svg.append("path")
     .datum({ type: "Sphere" })
@@ -48,130 +141,60 @@
     .attr("class", "graticule")
     .attr("d", geoPath);
 
-  function ready(error, world, migration) {
+  svg.selectAll('.land')
+    .data(land)
+    .enter().append('path')
+    .attr('class', 'land')
+    .attr('d', geoPath);
 
-    /*
-      topojson.feature converts
-      our RAW geo data into USEABLE geo data
-      always pass it data, then data.objects.___something___
-      then get .features out of it
-    */
-    const land = topojson.feature(world, world.objects.ne_50m_land).features;
-    const lakes = topojson.feature(world, world.objects.ne_50m_lakes).features;
-    const rivers = topojson.feature(world, world.objects.ne_50m_rivers_lake_centerlines).features;
+  svg.selectAll('.lakes')
+    .data(lakes)
+    .enter().append('path')
+    .attr('class', 'lakes')
+    .attr('d', geoPath)
+    .attr('fill', 'none');
 
-    /*
-      Add a path for each country
-      Shapes -> paths
-    */
-    svg.selectAll('.land')
-      .data(land)
-      .enter().append('path')
-      .attr('class', 'land')
-      .attr('d', geoPath);
+  svg.selectAll('.rivers')
+    .data(rivers)
+    .enter().append('path')
+    .attr('class', 'rivers')
+    .attr('d', geoPath)
+    .attr('fill', 'none');
 
-    svg.selectAll('.lakes')
-      .data(lakes)
-      .enter().append('path')
-      .attr('class', 'lakes')
-      .attr('d', geoPath)
-      .attr('fill', 'none');
+  const flightPaths = svg.append('path')
+    .attr('class', 'route')
+    .attr('d', geoPath(featureCollection.features[3]))
+    .attr('fill', 'none');
 
-    svg.selectAll('.rivers')
-      .data(rivers)
-      .enter().append('path')
-      .attr('class', 'rivers')
-      .attr('d', geoPath)
-      .attr('fill', 'none');
+  const turkeys = svg.append("circle")
+    .attr("class", "turkey")
+    .attr('cx', '0')
+    .attr('cy', '0')
+    .attr('r', '5');
 
-    const featureCollection = {
-      "type": "FeatureCollection",
-      "features": [ /* append featuresObj */]
-    };
-    const featuresObj = {
-      "type": "Feature",
-      "geometry": { /* append geometryObj */ },
-      "properties": { /* append propertiesObj */ }
-    };
-    const geometryObj = {
-      "type": "LineString",
-      "coordinates": [ /* longitutde */, /* latitude */]
-    };
-    const propertiesObj = {
-      "study_timezone": ""
-    };
-    const singleTurkey = migration.filter(function (d) {
-      return d.animal_id === 'Steamhouse 2';
-    });
-    const singleTurkeySorted = singleTurkey.sort(function (a, b) {
-      return d3.descending(parseInt(a.event_id), parseInt(b.event_id));
-    });
-    const nestedTurkey = d3.nest()
-      .key(function (d) { return d.animal_id; })
-      .entries(singleTurkeySorted);
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// Animate migration
+////////////////////////////////////////////////////////////////////////////////
 
-    nestedTurkey.forEach(function (turkey) {
-      let turkeyObj = {
-        "type": "Feature",
-        "geometry": { /* append geometryObj */ },
-        "properties": { /* append propertiesObj */ }
-      };
-      let movementObj = {
-        "type": "LineString",
-        "coordinates": []
-      };
-      let turkeyProperties = {
-        "study_timezone": ""
-      };
+  function transition(turkey, route) {
 
-      turkey.values.forEach(function (movement) {
-        movementObj.coordinates.push([+movement.longitude, +movement.latitude])
-      })
-
-      turkeyObj.geometry = movementObj;
-      turkeyProperties.study_timezone = turkey.values[0].study_timezone;
-      turkeyObj.properties = turkeyProperties;
-
-      featureCollection.features.push(turkeyObj)
-
-    });
-
-    const route = svg.append('path')
-      .attr('class', 'route')
-      .attr('d', geoPath(featureCollection))
-      .attr('fill', 'none');
-
-    function transition(turkey, route) {
-      var l = route.node().getTotalLength();
-      turkey.transition()
-        .duration(10000)
-        .attrTween("transform", delta(route.node()));
-
-    }
-
-    function delta(path) {
-      var l = path.getTotalLength();
-      return function (i) {
-        return function (t) {
-
-          var p = path.getPointAtLength(t * l);
-          // var x = p.x;
-          // var y = p.y;
-          // var r = 90 - Math.atan2(-y, x) * 180 / Math.PI;
-          // var s = Math.min(Math.sin(Math.PI * t) * 0.7, 0.3);
-          return `translate( ${p.x} , ${p.y} )`;
-        }
-      }
-    }
-
-    const turkey = svg.append("circle")
-      .attr("class", "turkey")
-      .attr('cx', '0')
-      .attr('cy', '0')
-      .attr('r', '10');
-
-    transition(turkey, route);
+    turkey.transition()
+      .duration(10000)
+      .attrTween("transform", delta(route.node()));
 
   }
 
-})()
+  function delta(path) {
+    var l = path.getTotalLength();
+
+    return function (i) {
+      return function (t) {
+        var p = path.getPointAtLength(t * l);
+        return `translate( ${p.x} , ${p.y} )`;
+      }
+    }
+  }
+
+  transition(turkeys, flightPaths);
+
+};
